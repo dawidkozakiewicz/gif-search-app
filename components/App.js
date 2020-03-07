d95ccc1c01d8de5e218aae6594167037c2006a01
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-03-07 23:27:54 +0100

    refact. code

--- components/App.js
+++ components/App.js
@@ -23,23 +23,16 @@ class App extends React.Component {
   };
 
   getGif = (searchingText, callback) => {
-    var GIPHY_PUB_KEY = "Tj0Zkn8M11H60GBwCg70bAUZ0IDvheBi";
-    var GIPHY_API_URL = "https://api.giphy.com";
-    var url =
-      GIPHY_API_URL +
-      "/v1/gifs/random?api_key=" +
-      GIPHY_PUB_KEY +
-      "&tag=" +
-      searchingText;
-    var xhr = new XMLHttpRequest();
+    const GIPHY_PUB_KEY = "Tj0Zkn8M11H60GBwCg70bAUZ0IDvheBi";
+    const GIPHY_API_URL = "https://api.giphy.com";
+    const url = `${GIPHY_API_URL}/v1/gifs/random?api_key=${GIPHY_PUB_KEY}&tag=${searchingText}`;
+    const xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.send();
     xhr.onload = function() {
       if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText).data;
-        console.log(data);
-        var gif = {
-          // url: data.fixed_width_downsampled_url,
+        const data = JSON.parse(xhr.responseText).data;
+        const gif = {
           url: data.images.downsized_large.url,
           sourceUrl: data.url
         };
